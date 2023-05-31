ef77d4e110509ba35a127127da9379e4ad1b1016
Antoine BARROUX <antoine.barroux@liksi.fr>
2023-05-31 11:31:33 +0200

    :wrench: adding config for climatiq api client

--- hexagonal-seminar-application/src/main/java/com/liksi/hexagonal/seminar/config/HexagonalSeminarConfig.java
+++ hexagonal-seminar-application/src/main/java/com/liksi/hexagonal/seminar/config/HexagonalSeminarConfig.java
@@ -1,15 +1,14 @@
 package com.liksi.hexagonal.seminar.config;
 
 import com.liksi.hexagonal.seminar.business.DummyObjectCrudOperationsService;
-import com.liksi.hexagonal.seminar.http.adapters.AirlabsApiClientImpl;
-import com.liksi.hexagonal.seminar.http.adapters.AirportHttpClient;
-import com.liksi.hexagonal.seminar.http.adapters.RouteHttpClient;
+import com.liksi.hexagonal.seminar.http.adapters.*;
 import com.liksi.hexagonal.seminar.http.mapper.AirportMapper;
 import com.liksi.hexagonal.seminar.http.mapper.RouteMapper;
 import com.liksi.hexagonal.seminar.jpa.adapters.DummyObjectJpaRepository;
 import com.liksi.hexagonal.seminar.jpa.adapters.DummyObjectRepositoryImpl;
 import com.liksi.hexagonal.seminar.jpa.mapper.DummyObjectMapper;
 import com.liksi.hexagonal.seminar.ports.http.AirlabsApiClient;
+import com.liksi.hexagonal.seminar.ports.http.ClimatiqApiClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -34,5 +33,10 @@ public class HexagonalSeminarConfig {
     @Bean
     public AirlabsApiClient airlabsApiClient(AirportHttpClient airportHttpClient, RouteHttpClient routeHttpClient, AirportMapper airportMapper, RouteMapper routeMapper) {
         return new AirlabsApiClientImpl(airportHttpClient, routeHttpClient, airportMapper, routeMapper);
+    }
+
+    @Bean
+    public ClimatiqApiClient climatiqApiClient(ClimatiqHttpClient climatiqHttpClient) {
+        return new ClimatiqApiClientImpl(climatiqHttpClient);
     }
 }
